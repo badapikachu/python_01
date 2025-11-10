eaa5c4ae5efd7530f04e772a1d7da6cf950e5d30
Sujal Sharma <158821111+badapikachu@users.noreply.github.com>
2025-11-10 07:39:20 +0530

    Add files via upload

---
+++ wordmeaningcode(9).py
@@ -0,0 +1,11 @@
+words= {"madad": "help",
+    "kursi": "chair",
+    "gajar":"carrot",
+    "insaan": "humans"}
+
+word= input("Enter the word you want meaning of: ")
+
+print(words[word])
+
+
+     
